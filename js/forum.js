9d70a476943565a8476bd5b17d6e65a624f777a2
kahsuann <kahsuan@hotmail.sg>
2025-12-03 00:15:36 +0800

    Add previous button to dialogues

--- js/forum.js
+++ js/forum.js
@@ -1,16 +1,31 @@
 const nextBtn = document.getElementById('next-btn');
+const prevBtn = document.getElementById('prev-btn');
 const postScript = document.getElementById('postscript');
 
 let position = 1;
+
 nextBtn.addEventListener('click', function() {
     const prevText = document.getElementById('text-' + position.toString())
     prevText.style.display = 'none';
     position++;
     if (position == 9) {
         nextBtn.style.display = 'none';
+        prevBtn.style.display = 'none';
         postScript.style.display = 'block';
     } else {
         const nextText = document.getElementById('text-' + position.toString())
         nextText.style.display = 'flex';
+        prevBtn.style.display = 'flex';
+    }
+});
+
+prevBtn.addEventListener('click', function() {
+    const currText = document.getElementById('text-' + position.toString())
+    currText.style.display = 'none';
+    position--;
+    const prevText = document.getElementById('text-' + position.toString())
+    prevText.style.display = 'flex';
+    if (position == 1) {
+        prevBtn.style.display = 'none';
     }
 });
